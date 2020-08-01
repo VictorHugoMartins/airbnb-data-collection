1cf50973f010b77c16adc71ac74a16084e875284
VictorHugoMartins <victor.martins1@aluno.ufop.edu.br>
2020-08-01 11:25:09 -0300

    Deleted test files

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-response = requests.get('https://www.airbnb.com.br/users/show/1033769')
-print(response.status_code)
-print(response)
-page = response.text
-print(page)             
